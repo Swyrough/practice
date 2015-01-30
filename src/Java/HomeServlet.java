c3fe822738b69c2bf83cc67fb1b13daa0bdef499
Vegetable26 <jhwang261@gmail.com>
2015-01-29 22:17:40 -0600

    Added XML mapping

--- src/Java/HomeServlet.java
+++ src/Java/HomeServlet.java
@@ -1,10 +1,34 @@
 package Java;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Timestamp;
+import com.google.appengine.api.datastore.*;
+
 
 /**
  * Created by compsci on 1/29/15.
  */
-public class HomeServlet {
+public class HomeServlet extends HttpServlet {
+
+    private DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
 
-    
+        java.util.Date date= new java.util.Date();
+        new Timestamp(date.getTime());
+
+
+
+                // eta
+                // location
+                // rating
+                // name
+
+    }
+
+
+
 }
